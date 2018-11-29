5c0f96d1555c03821454f2ac216cf2028c583c99
HOJO3 <joe.ho@oocl.com>
2018-11-29 11:29:11 +0800

    create interface

--- src/main/java/com/tw/apistackbase/api/EmployeeResource.java
+++ src/main/java/com/tw/apistackbase/api/EmployeeResource.java
@@ -16,6 +16,7 @@ public class EmployeeResource {
 
     @Autowired
     private EmployeeResource employeeService;
+    @Autowired
     private EmployeeRepository repository;
 
     @GetMapping(produces = {"application/json"})
