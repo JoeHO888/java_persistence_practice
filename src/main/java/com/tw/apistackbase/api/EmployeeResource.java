ed2adca9b6fe1076e17804fc05d58575ea423882
HOJO3 <joe.ho@oocl.com>
2018-11-29 11:28:56 +0800

    add respoisitory in controller

--- src/main/java/com/tw/apistackbase/api/EmployeeResource.java
+++ src/main/java/com/tw/apistackbase/api/EmployeeResource.java
@@ -1,5 +1,6 @@
 package com.tw.apistackbase.api;
 
+import com.tw.apistackbase.EmployeeRepository;
 import com.tw.apistackbase.core.Employee;
 import com.tw.apistackbase.application.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,7 +15,8 @@ public class EmployeeResource {
     private final Logger log = Logger.getLogger(this.getClass().getName());
 
     @Autowired
-    private EmployeeService employeeService;
+    private EmployeeResource employeeService;
+    private EmployeeRepository repository;
 
     @GetMapping(produces = {"application/json"})
     public List<Employee> list() {
@@ -24,5 +26,6 @@ public class EmployeeResource {
     @PostMapping(produces = {"application/json"})
     public void add(@RequestBody Employee employee) {
         employeeService.add(employee);
+        repository.save(employee);
     }
 }
